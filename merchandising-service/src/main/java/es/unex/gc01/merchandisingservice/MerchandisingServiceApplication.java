7f5df5e9326a808ceff8700fed3c1bf0d162c738
Abdel117 <awmahfoud@proton.me>
2025-10-02 14:31:22 +0200

    Inicialización de merchandising-service

---
+++ merchandising-service/src/main/java/es/unex/gc01/merchandisingservice/MerchandisingServiceApplication.java
@@ -0,0 +1,13 @@
+package es.unex.gc01.merchandisingservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MerchandisingServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MerchandisingServiceApplication.class, args);
+	}
+
+}

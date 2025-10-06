458b1d249edd9189aefb87e9e6204b81a6f9dda1
AlbertooCh <albertochamizo05@gmail.com>
2025-10-06 11:47:00 +0200

    Creación de openapi.yaml

--- src/main/java/es/unex/gc01/payments/PaymentsApplication.java
+++ src/main/java/es/unex/gc01/payments/PaymentsApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class PaymentsApplication {
 
     public static void main(String[] args) {
+
         SpringApplication.run(PaymentsApplication.class, args);
     }
 

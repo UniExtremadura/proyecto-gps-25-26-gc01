b588032656927dc9578c50e8b2768f7e01965a25
Abdel117 <awmahfoud@proton.me>
2025-10-17 04:59:04 +0200

    El servicio ahora gestiona compras.

---
+++ orders-service/src/main/java/es/unex/gc01/orders/OrdersServiceApplication.java
@@ -0,0 +1,13 @@
+package es.unex.gc01.orders;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrdersServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrdersServiceApplication.class, args);
+	}
+
+}
